f5cca425a73c7068892ea109a8e7c386f4268b31
Mateusz Kozieł <mateusz.koziel@instapage.com>
2021-07-24 17:21:38 +0200

    added scenario outline

--- cypress/integration/tivixTest.feature
+++ cypress/integration/tivixTest.feature
@@ -2,12 +2,13 @@ Feature: Car Rental in Tivix
 
   This feature is meant to help users to rent a car
 
-  Scenario: Successful rental of a car (happy path)
+  Scenario Outline: Successful rental of a car (happy path)
     Given user is on the Search page to rent a car
-    When user selects a Country in the dropdown
-    And user selects a City in the dropdown
-    And user types a Model in the textfield
-    And user selects pick-up and drop-off dates
+    When user selects a "<country>" in the country dropdown
+    And user selects a "<city>" in the city dropdown
+    And user types a "<model>" in the textfield
+    And user selects pick-up date as "2021-08-01"
+    And user selects drop-off date as "2021-08-07"
     And user clicks Search button
     Then results are displayed
 
@@ -15,8 +16,12 @@ Feature: Car Rental in Tivix
     And user proceeds further using Rent! button
     Then user is redirected to the summary of the rental
 
-    When user fills a textfield with a valid Name
-    And user fills a textfield with a valid LastName
-    And user fills a textfield with a valid Card Number
-    And user fills a textfield with a valid Email
+    When user fills a Name with "<name>"
+    And user fills a Last Name with "<lastname>"
+    And user fills a Card Number with "<cardnumber>"
+    And user fills an Email with "<email>"
     And user clicks Rent button
+
+    Examples:
+      | country | city | model | name | lastname | cardnumber | email |
+      | Poland | Wroclaw | Toyota | Mateusz | Koziel | 123123123 | matthewkoziel@gmail.com |
--- cypress/integration/tivixTest/tivixStepDef.js
+++ cypress/integration/tivixTest/tivixStepDef.js
@@ -3,18 +3,20 @@ import { Given,When,Then, And } from "cypress-cucumber-preprocessor/steps";
 Given("user is on the Search page to rent a car", () => {
   cy.visit("http://qalab.pl.tivixlabs.com/")
 })
-When("user selects a Country in the dropdown",()=> {
-  cy.get('#country').select('Poland')
+When("user selects a {string} in the country dropdown",(country1)=> {
+  cy.get('#country').select(country1);
 })
-And("user selects a City in the dropdown",()=> {
-  cy.get('#city').select('Wroclaw')
+And("user selects a {string} in the city dropdown",(city1)=> {
+  cy.get('#city').select(city1);
 })
-And("user types a Model in the textfield",()=> {
-  cy.get('#model').type('Toyota')
+And("user types a {string} in the textfield",(model1)=> {
+  cy.get('#model').type(model1)
 })
-And("user selects pick-up and drop-off dates",()=> {
-  cy.get('#pickup').type('2021-08-01')
-  cy.get('#dropoff').type('2021-08-07')
+And("user selects pick-up date as {string}",(pickUpDate)=> {
+  cy.get('#pickup').type(pickUpDate)
+})
+And("user selects drop-off date as {string}",(dropOffDate)=> {
+  cy.get('#dropoff').type(dropOffDate)
 })
 And("user clicks Search button",()=> {
   cy.get('.btn-primary').contains('Search').click()
@@ -32,17 +34,17 @@ Then("user is redirected to the summary of the rental", () => {
   cy.contains("Summary");
   cy.get(".col-md-4");
 })
-When("user fills a textfield with a valid Name", () => {
-  cy.get("#name").type('Mateusz');
+When("user fills a Name with {string}", (name) => {
+  cy.get("#name").type(name);
 })
-And("user fills a textfield with a valid LastName", () => {
-  cy.get("#last_name").type('Koziel');
+And("user fills a Last Name with {string}", (lastName) => {
+  cy.get("#last_name").type(lastName);
 })
-And("user fills a textfield with a valid Card Number", () => {
-  cy.get("#card_number").type('12312312312312312312312');
+And("user fills a Card Number with {string}", (cardNumber) => {
+  cy.get("#card_number").type(cardNumber);
 })
-And("user fills a textfield with a valid Email", () => {
-  cy.get("#email").type("randomemail@gmail.com");
+And("user fills an Email with {string}", (email) => {
+  cy.get("#email").type(email);
 })
 And("user clicks Rent button", () => {
   cy.get('.btn-primary').contains('Rent').click();
